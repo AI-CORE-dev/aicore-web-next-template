d9df70ead44c356955199bff68b18d4445cb595d
emiliano.caceres@aicore.com.ar <caceresmiranda05@gmail.com>
2024-08-28 11:43:53 -0300

    [FEATURE] add package fork-ts-checker-webpack-plugin - change file next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
     images: {
         domains: [],
     },
+    // typescript: {
+    //     ignoreBuildErrors: true,
+    // },
+    // eslint: {
+    //     ignoreDuringBuilds: true,
+    // },
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+            config.plugins.push(new ForkTsCheckerWebpackPlugin());
+        }
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
